77574f432afd82d93ec918605a4b7f57ddf9a9c4
Anthony <anthony.emile@gmail.com>
2022-09-08 08:53:17 +0200

    structure site ...

--- AngularAvatarGame/src/app/app.module.ts
+++ AngularAvatarGame/src/app/app.module.ts
@@ -2,15 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavigationComponent } from './config/navigation/navigation.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './routes';
+import { InventoryComponent } from './pages/inventory/inventory.component';
+import { CraftComponent } from './pages/craft/craft.component';
+import { MarketComponent } from './pages/market/market.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { GamesComponent } from './pages/games/games.component';
+import { NavigationTopComponent } from './config/navigation-top/navigation-top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
+    InventoryComponent,
+    CraftComponent,
+    MarketComponent,
+    GalleryComponent,
+    GamesComponent,
+    NavigationTopComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
