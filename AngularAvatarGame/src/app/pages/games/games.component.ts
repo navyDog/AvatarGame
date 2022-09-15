e4b49f993f0082ccdf75e15f6832aad5e2ceefc8
navyDog <jbabkine@yahoo.com>
2022-09-15 09:25:14 +0200

    jeu

--- AngularAvatarGame/src/app/pages/games/games.component.ts
+++ AngularAvatarGame/src/app/pages/games/games.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Route, Router } from "@angular/router";
+import { UserService } from "src/app/config/services/user.service";
 
 @Component({
   selector: "app-games",
@@ -6,10 +8,27 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./games.component.css"],
 })
 export class GamesComponent implements OnInit {
-  constructor() {}
+  game1: String;
+
+
+  constructor(
+    private ar: ActivatedRoute,
+    private userService: UserService,
+    private router: Router
+  ) {
+    this.game1=new String;
+  }
 
   ngOnInit(): void {
     /*var coin = require('coin-toss');
     console.log(coin.toss());*/
+  }
+
+  public jouer() {
+    this.ar.paramMap.subscribe((params) => {
+      this.userService.game1().subscribe((result) => {
+        console.log(result);
+      })
+    })
   }
 }
