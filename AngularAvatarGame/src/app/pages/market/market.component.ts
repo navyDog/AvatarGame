6202840ee92c64f7e9bb33027b4a551f29c3ca98
Anthony <anthony.emile@gmail.com>
2022-09-15 15:04:10 +0200

    inventory_market_part1

--- AngularAvatarGame/src/app/pages/market/market.component.ts
+++ AngularAvatarGame/src/app/pages/market/market.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Items } from "src/app/entities/items";
 
 @Component({
-  selector: 'app-market',
-  templateUrl: './market.component.html',
-  styleUrls: ['./market.component.css']
+  selector: "app-market",
+  templateUrl: "./market.component.html",
+  styleUrls: ["./market.component.css"],
 })
 export class MarketComponent implements OnInit {
+  private _itemToBuy: Items = new Items();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    // GET ALL ITEMS ON SALE OF ALL PLAYERS
   }
 
+  public get itemToBuy(): Items {
+    return this._itemToBuy;
+  }
+  public set itemToBuy(value: Items) {
+    this._itemToBuy = value;
+  }
+
+  public eraseItemToBuy() {
+    this.itemToBuy = new Items();
+  }
+
+  public addItemToBuy(item: Items) {
+    this.itemToBuy = item;
+  }
 }
