67f22344322a9ed6e4a7f7419eb06a8b7cd7897c
Anthony <anthony.emile@gmail.com>
2022-09-15 09:08:01 +0200

    market_galleries_games

--- AngularAvatarGame/src/assets/js/main.js
+++ AngularAvatarGame/src/assets/js/main.js
@@ -35,7 +35,7 @@ function inventorySold(p) {
   /* let x1 = document.querySelector("#craftContent");
   let x2 = document.querySelector("#note");*/
   let y = document.querySelector("#invSoldItem");
-  let z = document.querySelector("#PriceItemInput")
+  let z = document.querySelector("#PriceItemInput");
 
   if (p == "on") {
     /*x1.style.filter = "blur(4px)";
@@ -120,21 +120,85 @@ function craftItemSelect(p) {
 }
 
 // ADD IMAGE TO THE CRAFTED AVATAR
-/*function craftAddSelectedImg(p, q) {
-  let x;
-  if (p == "head") {
-    x = document.getElementById("craftAvatarHead");
-  } else if (p == "hand_right") {
-    x = document.getElementById("craftAvatarHead");
-  } else if (p == "body") {
-    x = document.getElementById("craftAvatarHead");
-  } else if (p == "hand_left") {
-    x = document.getElementById("craftAvatarHead");
-  } else if (p == "feet_right") {
-    x = document.getElementById("craftAvatarHead");
-  } else if (p == "feet_left") {
-    x = document.getElementById("craftAvatarHead");
+function marketBuyItem(p) {
+  let x = document.querySelector("#marMarketItems");
+  let y = document.querySelector("#marUsersItems");
+  let z = document.querySelector("#marBuyPopup");
+
+  if (p == "on") {
+    x.style.filter = "blur(4px)";
+    y.style.filter = "blur(4px)";
+    z.style.display = "flex";
+  } else if (p == "off") {
+    x.style.filter = "blur(0px)";
+    y.style.filter = "blur(0px)";
+    z.style.display = "none";
   }
-  x.src = "assets/items/" + q + ".png";
-  console.log(q);
-}*/
+}
+
+function marketAccordion(p, q) {
+  let a1 = "marContentTop";
+  let a2 = "marContentBot";
+  let x1, x2, x3, x4, x5, x6, x7, y1, y2, y3, y4, y5, y6, y7, z1, z2;
+  if (p == "top") {
+    x1 = document.getElementById("marContentTopHead");
+    x2 = document.getElementById("marContentTopBody");
+    x3 = document.getElementById("marContentTopHandRight");
+    x4 = document.getElementById("marContentTopHandLeft");
+    x5 = document.getElementById("marContentTopFeetRight");
+    x6 = document.getElementById("marContentTopFeetLeft");
+    x7 = document.getElementById("marContentTopAvatar");
+    y1 = document.getElementById("marContentTopHeadBtn");
+    y2 = document.getElementById("marContentTopBodyBtn");
+    y3 = document.getElementById("marContentTopHandRightBtn");
+    y4 = document.getElementById("marContentTopHandLeftBtn");
+    y5 = document.getElementById("marContentTopFeetRightBtn");
+    y6 = document.getElementById("marContentTopFeetLeftBtn");
+    y7 = document.getElementById("marContentTopAvatarBtn");
+    z1 = document.getElementsByClassName("marContentTopBtn");
+    z2 = document.getElementsByClassName("marContentTop");
+  } else if (p == "bot") {
+    x1 = document.getElementById("marContentBotHead");
+    x2 = document.getElementById("marContentBotBody");
+    x3 = document.getElementById("marContentBotHandRight");
+    x4 = document.getElementById("marContentBotHandLeft");
+    x5 = document.getElementById("marContentBotFeetRight");
+    x6 = document.getElementById("marContentBotFeetLeft");
+    x7 = document.getElementById("marContentBotAvatar");
+    y1 = document.getElementById("marContentBotHeadBtn");
+    y2 = document.getElementById("marContentBotBodyBtn");
+    y3 = document.getElementById("marContentBotHandRightBtn");
+    y4 = document.getElementById("marContentBotHandLeftBtn");
+    y5 = document.getElementById("marContentBotFeetRightBtn");
+    y6 = document.getElementById("marContentBotFeetLeftBtn");
+    y7 = document.getElementById("marContentBotAvatarBtn");
+    z1 = document.getElementsByClassName("marContentBotBtn");
+    z2 = document.getElementsByClassName("marContentBot");
+  }
+  for (let i = 0; i < z1.length; i++) {
+    z1[i].classList.remove("active");
+    z2[i].style.display = "none";
+  }
+  if (q == "head") {
+    x1.style.display = "flex";
+    y1.classList.add("active");
+  } else if (q == "body") {
+    x2.style.display = "flex";
+    y2.classList.add("active");
+  } else if (q == "handRight") {
+    x3.style.display = "flex";
+    y3.classList.add("active");
+  } else if (q == "handLeft") {
+    x4.style.display = "flex";
+    y4.classList.add("active");
+  } else if (q == "feetRight") {
+    x5.style.display = "flex";
+    y5.classList.add("active");
+  } else if (q == "feetLeft") {
+    x6.style.display = "flex";
+    y6.classList.add("active");
+  } else if (q == "avatars") {
+    x7.style.display = "flex";
+    y7.classList.add("active");
+  }
+}
